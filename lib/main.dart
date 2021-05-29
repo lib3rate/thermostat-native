3901600072daf34ff82648b2f82744b70547d6ae
Dmitry Petrov <dmitry.petrov0918@gmail.com>
2021-05-28 23:49:41 -0400

    Fix rendering form on login page

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class App extends StatelessWidget {
         primarySwatch: Colors.cyan,
       ),
       home: LoginPage(),
-      // home: HomePage(title: 'Thermostat Home'),
     );
   }
 }
@@ -23,6 +22,29 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Center(
+        child: LoginForm(),
+      ),
+    );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  final Function login = () {};
+
+  @override
+  _LoginFormState createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+
   void _login() {
     Navigator.of(context).push(MaterialPageRoute<void>(
       builder: (BuildContext context) {
@@ -33,12 +55,45 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body: Center(
-        child: LoginForm(),
+    return Form(
+      key: _formKey,
+      // padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Enter your username'),
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your username';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Enter your password'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter your password';
+              }
+              return null;
+            },
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Validate returns true if the form is valid, or false otherwise.
+              if (_formKey.currentState!.validate()) {
+                _login();
+              }
+            },
+            child: Text('Submit'),
+          ),
+        ],
       ),
     );
   }
@@ -73,27 +128,6 @@ class _HomePageState extends State<HomePage> {
     setState(() => _counter--);
   }
 
-  void _login() {
-    Navigator.of(context).push(MaterialPageRoute<void>(
-      builder: (BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('Login'),
-          ),
-          // body: Center(
-          //   child: Column(
-          //     mainAxisAlignment: MainAxisAlignment.center,
-          //     children: [],
-          //   ),
-          // )
-          body: Center(
-            child: LoginForm(),
-          ),
-        );
-      },
-    ));
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -107,7 +141,8 @@ class _HomePageState extends State<HomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
-        actions: [IconButton(onPressed: _login, icon: Icon(Icons.list))],
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.list))],
+        // actions: [IconButton(onPressed: _login, icon: Icon(Icons.list))],
       ),
       body: Center(
         child: Column(
@@ -122,6 +157,7 @@ class _HomePageState extends State<HomePage> {
                 onPressed: _decreaseDesiredTemperature,
                 tooltip: 'Decrease desired temperature',
                 child: Icon(Icons.remove),
+                heroTag: "decrease temperature button",
               ),
               Text(
                 '$_counter°',
@@ -131,66 +167,11 @@ class _HomePageState extends State<HomePage> {
                 onPressed: _increaseDesiredTemperature,
                 tooltip: 'Increase desired temperature',
                 child: Icon(Icons.add),
+                heroTag: "increase temperature button",
               ),
             ]),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class LoginForm extends StatefulWidget {
-  @override
-  _LoginFormState createState() => _LoginFormState();
-}
-
-class _LoginFormState extends State<LoginForm> {
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: 'Enter your username'),
-            // The validator receives the text that the user has entered.
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your username';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: 'Enter your password'),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter your password';
-              }
-              return null;
-            },
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // Validate returns true if the form is valid, or false otherwise.
-              if (_formKey.currentState!.validate()) {
-                // _login();
-                // If the form is valid, display a snackbar. In the real world,
-                // you'd often call a server or save the information in a database.
-                ScaffoldMessenger.of(context)
-                    .showSnackBar(SnackBar(content: Text('Processing Data')));
-              }
-            },
-            child: Text('Submit'),
-          ),
-        ],
       ),
     );
   }
