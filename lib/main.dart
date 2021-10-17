14abc05a9323ab5e3888cc3a8ed48afb3b28b90d
Dmitry Petrov <dmitry.petrov0918@gmail.com>
2021-10-17 17:03:14 -0400

    Add Redux package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
 import 'components/Login/index.dart';
-
 void main() => runApp(App());
-
 class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
